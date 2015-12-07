6f8b688a25cf349bfdcf89eb95e43592df5caf92
Andrei Salomatov <andrei.salomatov@gmail.com>
2015-12-07 13:51:03 -0500

    annotation

--- denovo_analysis/annSnpEff.mk
+++ denovo_analysis/annSnpEff.mk
@@ -26,7 +26,8 @@ $(outFile): $(inFile)
 	$(JAVA) -Xmx5G -jar $(SNPSIFTJAR) annotate -id -c $(SNPEFFCONF) -noLog -dbsnp $< | \
         $(JAVA) -Xmx5G -jar $(SNPSIFTJAR) varType -c $(SNPEFFCONF) -noLog - | \
 		$(JAVA) -Xmx5G -jar $(SNPEFFJAR) ann -noLog -c $(SNPEFFCONF) $(SNPEFFGENOME) -v -csvStats -s $(sumFile) | \
-		$(BGZIP) -c > $@
+		$(JAVA) -Xmx5G -jar $(SNPSIFTJAR)  dbnsfp - -a -c $(SNPEFFCONF) -db $(DBNSFP) \
+		-f ref,alt,aaref,aaalt,rs_dbSNP142,genename,Ensembl_geneid,Ensembl_transcriptid,Ensembl_proteinid,SIFT_score,SIFT_pred,Polyphen2_HDIV_score,Polyphen2_HDIV_pred,Polyphen2_HVAR_score,Polyphen2_HVAR_pred,MutationTaster_score,MutationTaster_pred,MutationAssessor_score,MutationAssessor_pred,PROVEAN_score,PROVEAN_pred,CADD_raw,CADD_raw_rankscore,CADD_phred,GERP++_NR,GERP++_RS,GERP++_RS_rankscore,1000Gp3_AC,1000Gp3_AF,ExAC_AC,ExAC_AF | sed "s/dbNSFP_GERP++/dbNSFP_GERP/g" | $(BGZIP) -c > $@
 	$(TABIX) -f -p vcf $@
 
 
